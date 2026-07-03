a48e2e8608495b91badbbd904a6ead9de23e3f56
PavaniLanka22 <pavanilanka2122@gmail.com>
2026-07-03 13:21:33 +0530

    Env variable modfication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://127.0.0.1:5000", {
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
   transports: ["websocket", "polling"],
 });
 
